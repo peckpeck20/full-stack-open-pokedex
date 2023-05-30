17102324d567dfeb3e7409a00db0af6497c90dfa
Jose Zapata <peckpeckthebeast@gmail.com>
2023-05-30 17:40:38 +0300

    fix linter & run prettier

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 6000;
 
-app.use(express.static("dist"));
+app.use(express.static('dist'));
 
 app.listen(PORT, () => {
-  console.log("server started on port:" + PORT);
+  console.log('server started on port:' + PORT);
 });
