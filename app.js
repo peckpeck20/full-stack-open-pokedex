c653bcd8b43456493abe0a581b831d578a82ee0c
Jose Zapata <peckpeckthebeast@gmail.com>
2023-05-31 17:32:32 +0300

    revert breaking change

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const app = express();
 
-// Heroku dynamically sets a port
 const PORT = process.env.PORT || 6000;
 
 app.use(express.static('dist'));
 
 app.get('/health', (_, res) => {
-  throw 'error...';
-  // eslint-disable-next-line no-unreachable
   res.send('ok');
 });
 
