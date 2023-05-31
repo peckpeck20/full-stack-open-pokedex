d19a541d2022dd4e9785232d2f25bcd38b68317d
Jose Zapata <peckpeckthebeast@gmail.com>
2023-05-31 17:28:09 +0300

    add breaking change

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 6000;
 app.use(express.static('dist'));
 
 app.get('/health', (_, res) => {
+  throw 'error...';
+  // eslint-disable-next-line no-unreachable
   res.send('ok');
 });
 
